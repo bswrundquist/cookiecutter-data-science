7462376909a0388d7e2bc13a368e19879aa882f3
brundquist <brandon.rundquist@davita.com>
2019-03-28 04:33:31 -0600

    echo post_gen

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME='{{ cookiecutter.project_name }}'
+echo "Initializing project $PROJECT_NAME"
 
 sudo apt -y install python3-pip
 sudo pip3 install dvc[gs]
 sudo pip3 install cookiecutter
 
+echo "Setting up code versioning"
 git init
 git config --global credential.https://source.developers.google.com.helper gcloud.sh
 git remote add origin https://source.developers.google.com/p/esai-222021/r/$PROJECT_NAME
 git push --set-upstream origin master
 
+echo "Setting up data versioning"
 dvc init
 git commit -m 'initialize DVC'
 
