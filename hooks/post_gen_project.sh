b5ac5e09b53b9343012602900cb9eddd56cc4bbb
brundquist <brandon.rundquist@davita.com>
2019-03-28 04:25:26 -0600

    full post_gen

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,15 +3,19 @@
 PROJECT_NAME='{{ cookiecutter.project_name }}'
 
 sudo apt -y install python3-pip
-suddo pip3 install dvc[gs]
+sudo pip3 install dvc[gs]
+sudo pip3 install cookiecutter
 
-# set env variable ENV = develop, train
+git init
+git config --global credential.https://source.developers.google.com.helper gcloud.sh
+git remote add origin https://source.developers.google.com/p/esai-222021/r/$PROJECT_NAME
+git push --set-upstream origin master
+
 dvc init
 git commit -m 'initialize DVC'
 
-dvc remote add -d data gs://$PROJECT_NAME/data
 dvc remote add -d checkpoints gs://$PROJECT_NAME/checkpoints
+dvc remote add -d data gs://$PROJECT_NAME/data
+git add .dvc/config
 git commit -m 'add data and checkpoints to dvc'
 git push
-
-
